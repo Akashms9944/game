2d1ca2df7d477f8c73e3351051009d9a372cde78
Akash <m.s.akash9944@gmail.com>
2026-05-28 10:50:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const box = document.getElementById("box");
+const gameArea = document.getElementById("game-area");
+const scoreDisplay = document.getElementById("score");
+const startBtn = document.getElementById("start-btn");
+
+let score = 0;
+let gameInterval;
+
+function moveBox() {
+  const maxX = gameArea.clientWidth - box.clientWidth;
+  const maxY = gameArea.clientHeight - box.clientHeight;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  box.style.left = randomX + "px";
+  box.style.top = randomY + "px";
+}
+
+box.addEventListener("click", () => {
+  score++;
+  scoreDisplay.textContent = score;
+  moveBox();
+});
+
+startBtn.addEventListener("click", () => {
+  score = 0;
+  scoreDisplay.textContent = score;
+
+  clearInterval(gameInterval);
+
+  gameInterval = setInterval(moveBox, 1000);
+});
